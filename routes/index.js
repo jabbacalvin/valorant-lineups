db036678bef38c54f8464c65d07647080169e333
Calvin Hsu <jabbacalvin@gmail.com>
2023-07-10 13:14:39 -0500

    included oauth

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+  }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/movies',
+    failureRedirect: '/movies'
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function (req, res) {
+  req.logout(function () {
+    res.redirect('/movies');
+  });
+});
+
 module.exports = router;
